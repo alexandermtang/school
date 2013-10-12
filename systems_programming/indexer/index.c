8a458f87e7a6659d544fc91fdf5cc8a24f8fe5bb
Alex Tang <alex.m.tang@gmail.com>
2013-10-12 18:00:57 -0400

    Fix tokenizer and makefile

--- systems_programming/indexer/index.c
+++ systems_programming/indexer/index.c
@@ -10,23 +10,26 @@ typedef struct Term {
 
 int main(int argc, char *argv[])
 {
-    FILE *fp;
+    FILE *fp, *ofp;
     char *line = NULL;
     size_t len = 0;
     ssize_t read;
 
     fp = fopen("sorted-list.c", "r");
+    ofp = fopen("asdf", "w");
     if (fp == NULL)
       exit(1);
 
     while ((read = getline(&line, &len, fp)) != -1) {
       printf("Line length %zu :\n", read);
       printf("%s", line);
+      fprintf(ofp, "%s", line);
     }
 
     if (line)
         free(line);
 
     fclose(fp);
+    fclose(ofp);
     exit(1);
 }
--- systems_programming/indexer/tokenizer.c
+++ systems_programming/indexer/tokenizer.c
@@ -15,45 +15,45 @@
 
 struct TokenizerT_ {
 	char* copied_string;
-	char* delimiters;		
+	char* delimiters;
 	char* current_position;
 };
 
 typedef struct TokenizerT_ TokenizerT;
 
 char is_escape_character(char character) {
-	
+
 	/*
 	 * Description: determines if a character is a member of the set of escaped characters
 	 * Parameters: character to be evaluated
 	 * Modifies: nothing
 	 * Returns: appropriate escape character sequence if it is a member, 0 if it is not
-	 * 
+	 *
 	 */
 
 	char* escape_sequence = "ntvbrfa\\?'\"";
 	char* escape_characters = "\n\t\v\b\r\f\a\\\?\'\"";
 	int offset = 0;
-	
+
 	for(offset = 0; offset < strlen(escape_sequence); offset++) {
 		if(*(escape_sequence + offset) == character) {
 			return *(escape_characters + offset);
 		}
 	}
-	
+
 	return 0;
 }
- 
+
 int char_to_hex(char character) {
-	
+
 	/*
 	 * Description: converts a hex digit represented as a character into its actual integer value
 	 * Parameters: character to be converted
 	 * Modifies: nothing
 	 * Returns: value of the hex digit character as an integer
-	 * 
+	 *
 	 */
-	
+
 	if(isdigit(character)){
 		return character - '0';
 	} else if (islower(character)){
@@ -61,8 +61,8 @@ int char_to_hex(char character) {
 	} else {
 		return character - 'A' + 10;
 	}
-} 
- 
+}
+
 int char_to_oct(char character) {
 
 	/*
@@ -70,46 +70,46 @@ int char_to_oct(char character) {
 	 * Parameters: character to be converted
 	 * Modifies: nothing
 	 * Returns: value of the octal digit character as an integer
-	 * 
+	 *
 	 */
 
 	return character - '0';
-} 
- 
+}
+
 int is_oct_digit(char oct_digit) {
-	
+
 	/*
 	 * Description: determines if a character represents a octal digit
 	 * Parameters: character to be evaluated
 	 * Modifies: nothing
 	 * Returns: 1 if it is an octal character, 0 if it is not
-	 * 
+	 *
 	 */
-	
+
 	if(oct_digit >= '0' && oct_digit <= '7') {
 		return 1;
 	} else {
 		return 0;
 	}
 }
- 
- 
+
+
 char* unescape_string(char* string) {
-	
+
 	/*
 	 * Description: given an input stream converts escaped characters into their appropriate char representation and returns it as a new string
 	 * Parameters: string to be converted
 	 * Modifies: nothing
 	 * Returns: resulting string with all the escaped characters with their appropriate values
-	 * 
+	 *
 	 */
 
 	char* unescaped_string = (char*)malloc(strlen(string) * sizeof(char) + 1);
 	int current_position = 0;
 	int unescaped_string_position = 0;
-	unsigned char escape_character = 0;	
-	
-	for(current_position = 0; current_position < strlen(string); current_position++) {	
+	unsigned char escape_character = 0;
+
+	for(current_position = 0; current_position < strlen(string); current_position++) {
 			escape_character = *(string + current_position);
 			if(*(string + current_position) == '\\') {
 				if(*(string + current_position + 1) == 'x') {
@@ -136,7 +136,7 @@ char* unescape_string(char* string) {
 					current_position += oct_count;
 				} else {
 					escape_character = is_escape_character(*(string + current_position + 1));
-					
+
 					if(escape_character == 0) {
 						escape_character = *(string + current_position);
 					} else {
@@ -148,9 +148,9 @@ char* unescape_string(char* string) {
 			unescaped_string_position++;
 			escape_character = 0;
 	}
-	
+
 	*(unescaped_string + unescaped_string_position) = '\0';
-	
+
 	return unescaped_string;
 }
 
@@ -160,7 +160,7 @@ char* unescape_string(char* string) {
 /*
  * TKCreate creates a new TokenizerT object for a given set of separator
  * characters (given as a string) and a token stream (given as a string).
- * 
+ *
  * TKCreate should copy the two arguments so that it is not dependent on
  * them staying immutable after returning.  (In the future, this may change
  * to increase efficiency.)
@@ -172,29 +172,29 @@ char* unescape_string(char* string) {
  */
 
 TokenizerT *TKCreate(char *separators, char *ts) {
-	
+
 	/*
 	 * Description: creates a new tokenizer struct from the token stream and delimiters
 	 * Parameters: set of delimiters, token stream
 	 * Modifies: nothing
 	 * Returns: a pointer to a tokenizer struct on success, a null pointer on failure
-	 * 
+	 *
 	 */
-	 
+
 	if(separators == NULL || ts == NULL){
 		return NULL;
 	}
-	
+
 	TokenizerT* tokenizer = (TokenizerT*)malloc(sizeof(TokenizerT));
-	
+
 	if(tokenizer == NULL){
 		return NULL;
 	}
-	
+
 	tokenizer->delimiters = unescape_string(separators);
 	tokenizer->copied_string = unescape_string(ts);
 	tokenizer->current_position = tokenizer->copied_string;
-	
+
 	return tokenizer;
 }
 
@@ -205,34 +205,34 @@ TokenizerT *TKCreate(char *separators, char *ts) {
  * You need to fill in this function as part of your implementation.
  */
 
-void TKDestroy(TokenizerT *tk) {	
-	
+void TKDestroy(TokenizerT *tk) {
+
 	/*
 	 * Description: destroys tokenizer struct and deallocates all memory
 	 * Parameters: tokenizer to be destroyed
 	 * Modifies: tokenizer struct: deallocates memory
-	 * Returns: nothing 
+	 * Returns: nothing
 	 */
-	 
+
 	free(tk->copied_string);
 	free(tk->delimiters);
 	free(tk);
-	
+
 	return;
 }
 
 
 char is_delimiter(char character, char* delimiters) {
-	
+
 	/*
 	 * Description: determines if a particular character is a member of the set of delimiters
 	 * Parameters: character to be compared, string of delimiters
 	 * Modifies: Nothing
 	 * Returns: 1 if character is a delimiter, 0 if it is not
 	 */
-	
+
 	char* current = NULL;
-	
+
 	for(current = delimiters; current - delimiters < strlen(delimiters); current++) {
 		if(character == *current) {
 			return 1;
@@ -259,32 +259,32 @@ char *TKGetNextToken(TokenizerT *tk) {
 	/*
 	 * Description: returns the next token from the token stream specified within the tokenizer
 	 * Parameters: tokenizer from which to extract token
-	 * Modifies: tokenizer->current_position: identifies starting point of next token; creates a new string with 
+	 * Modifies: tokenizer->current_position: identifies starting point of next token; creates a new string with
 	 * Returns: token extracted as a char* on success, null on failure/end of string;
 	 */
-	
+
 	char* token = NULL;
 	char* token_start = NULL;
 
 	while(tk->current_position - tk->copied_string < strlen(tk->copied_string)) {
 		if(!is_delimiter(*tk->current_position, tk->delimiters)) {
-		
+
 			token_start = tk->current_position;
 			break;
 		}
 		tk->current_position++;
 	}
-	
+
 	if(token_start == NULL) {
 		return NULL;
 	}
-	
+
 	while(tk->current_position - tk->copied_string < strlen(tk->copied_string)) {
 		if(is_delimiter(*tk->current_position, tk->delimiters)) {
 			break;
 		}
 		tk->current_position++;
-	}	
+	}
 
 	token = (char*)malloc(sizeof(char) * (tk->current_position - token_start + 1));
 	strncpy(token, token_start, tk->current_position - token_start);
@@ -300,27 +300,27 @@ char *TKGetNextToken(TokenizerT *tk) {
  * Each token should be printed on a separate line.
  */
 
-int main(int argc, char **argv) {
-	
-	if(argc != 3){
-		printf("Error: invalid number of arguments\n");
-		return -1;
-	}
-	
-	TokenizerT* tokenizer = TKCreate(argv[1], argv[2]);
-	
-	if(tokenizer == NULL) {
-		printf("Error: unable to create tokenizer\n");
-	}
-	
-	char* token = NULL;
-	
-	while((token = TKGetNextToken(tokenizer)) != NULL) {
-		printf("%s\n", token);
-		free(token);
-	}
-	
-	TKDestroy(tokenizer);
+/*int main(int argc, char **argv) {*/
 
-	return 0;
-}
+	/*if(argc != 3){*/
+		/*printf("Error: invalid number of arguments\n");*/
+		/*return -1;*/
+	/*}*/
+
+	/*TokenizerT* tokenizer = TKCreate(argv[1], argv[2]);*/
+
+	/*if(tokenizer == NULL) {*/
+		/*printf("Error: unable to create tokenizer\n");*/
+	/*}*/
+
+	/*char* token = NULL;*/
+
+	/*while((token = TKGetNextToken(tokenizer)) != NULL) {*/
+		/*printf("%s\n", token);*/
+		/*free(token);*/
+	/*}*/
+
+	/*TKDestroy(tokenizer);*/
+
+	/*return 0;*/
+/*}*/
