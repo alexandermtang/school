84d1ee5509801ff408d1adcda40c84f7a9386705
Alex Tang <alex.m.tang@gmail.com>
2013-12-06 17:44:22 -0500

    main file

--- systems_programming/malloc_and_free/malloc_and_free.c
+++ systems_programming/malloc_and_free/malloc_and_free.c
@@ -1,5 +1,27 @@
-#define malloc(x) mymalloc(x, __FILE__, __LINE__)
-#define free(x) myfree(x, __FILE__, __LINE__)
+#include <stdlib.h>
+#include <stdio.h>
 
-void * mymalloc(x, __FILE__, __LINE__);
-void * myfree(x, __FILE__, __LINE__);
+#define malloc(x) my_malloc(x)
+#define free(x) my_free(x)
+#define BLOCKSIZE 5000
+
+void * my_malloc(unsigned int size);
+void * my_free(void *p);
+
+void * my_malloc(unsigned int size)
+{
+
+  return NULL;
+}
+
+void * my_free(void *p)
+{
+
+  return NULL;
+}
+
+int main(void)
+{
+
+  exit(0);
+}
